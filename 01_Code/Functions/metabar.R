2c851ac18da58e5844970807eea1223bac9b73c4
Bourret <audrey.bourret@usherbrooke.ca>
2022-04-13 12:42:10 -0400

    Add part to fit with correction by project #10

--- 01_Code/Functions/metabar.R
+++ 01_Code/Functions/metabar.R
@@ -31,7 +31,7 @@ ggpcrplate.modif <- function (metabarlist, legend_title = "well_values", FUN = f
     ggplot(plate_design, aes(y = match(.data$plate_row, 
                                        LETTERS[1:8]), x = .data$plate_col)) + 
       geom_tile(aes(fill = .data$control_type), height = 1, width = 1, na.rm = TRUE) + 
-      facet_wrap(~.data$plate_no, scales = "free") + theme_bw() + 
+      facet_wrap(~.data$plate_no, scales = "free", ncol = 1) + theme_bw() + 
       scale_y_reverse(breaks = 1:8, labels = LETTERS[1:8]) + 
       scale_x_continuous(breaks = 1:12) + 
       scale_fill_manual(breaks = c("sample", "positive", "extraction", "pcr", "sequencing"), values = c("darkgray", "cyan4", "brown", "red", "pink"), na.value = "darkgrey", na.translate = FALSE) + 
@@ -45,14 +45,24 @@ ggpcrplate.modif <- function (metabarlist, legend_title = "well_values", FUN = f
 
 ggpcrplate.cont <- function(metabarlist, N = Inf){
   
-  motus.common_contam <- metabarlist$motus %>% filter(not_a_all_conta == F | not_a_max_conta == F)%>% arrange(desc(count)) %>% head(N)
-  reads.common_contam <- metabarlist$reads[,rownames(motus.common_contam)]
+  #reads.common_contam <- NULL
+  
+ # if(level == "pcr"){
+  motus.common_contam <- metabarlist$motus %>% filter(not_a_max_conta == F) %>% arrange(desc(count)) %>% head(N)
+  reads.common_contam <- metabarlist$reads[,rownames(motus.common_contam)] 
+  #}
+  
+  #if(level == "extraction"){
+  #  motus.common_contam <- metabarlist$motus %>% filter(not_an_extraction_all_conta == F | not_an_extraction_max_conta == F)%>% arrange(desc(count)) %>% head(N)
+ #  reads.common_contam <- metabarlist$reads[,rownames(motus.common_contam)] 
+ # }
+
   
   if(is.null(nrow(reads.common_contam))) stop("No figure because there is 1 or less contaminant")
   
   metabarlist_contam <- metabarlist_generator(reads.common_contam, motus.common_contam, metabarlist$pcrs, metabarlist$samples)
   
-  title.int = ifelse(N > length(rownames(motus.common_contam)), "ALL contaminants", paste(N, "<br>most abundant contaminants") )
+  title.int = ifelse(N > length(rownames(motus.common_contam)), "ALL contaminants", paste(N, "highest") )
   
   ggpcrplate.modif(metabarlist_contam, legend_title = title.int,
                    FUN = function(m){rowSums(m$reads)})
